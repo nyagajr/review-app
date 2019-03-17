33d43fbe574fc50b04e14eaa092cc6812f670e1f
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 08:24:19 +0300

    adding project model with titlte and image

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    Title = models.CharField(max_length =30)
+    project_img = models.CharField(max_length =30)
 class profile(models.Model):
     name = models.CharField(max_length =30)
     dpic = models.CharField(max_length =30)
