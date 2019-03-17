8504ab9ed35f431d542199cd966b47337e556126
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 08:33:08 +0300

    returning a string representation of our model.

--- app/models.py
+++ app/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 # Create your models here.
 class project(models.Model):
-    Title = models.CharField(max_length =30)
+    title = models.CharField(max_length =30)
     project_img = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
     link = models.CharField(max_length =30)
 
+    def __str__(self):
+        return self.title
 
 class profile(models.Model):
     u_name = models.CharField(max_length =30)
     dpic = models.CharField(max_length =30)
     contact_info = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.u_name
