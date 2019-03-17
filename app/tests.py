35ff1a0409c2fc1c2f43278718fe53d4cb20e296
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 09:51:02 +0300

    app tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import profile, project
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.junior=profile(u_name = 'junior', dpic = 'lol.jpg',contact_info = 'mee')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.junior,profile))
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.junior=project(title = 'junior', project_img = 'lol.jpg',description = 'mee', link = 'you.com')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.junior,project))
