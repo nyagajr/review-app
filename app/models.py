d8eecda6e41375fdab123f635e498631e7ab9145
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 08:40:21 +0300

    linking to a foreign key

--- app/models.py
+++ app/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class project(models.Model):
-    title = models.CharField(max_length =30)
-    project_img = models.CharField(max_length =30)
-    description = models.CharField(max_length =30)
-    link = models.CharField(max_length =30)
-
-    def __str__(self):
-        return self.title
-
 class profile(models.Model):
     u_name = models.CharField(max_length =30)
     dpic = models.CharField(max_length =30)
@@ -17,3 +8,15 @@ class profile(models.Model):
 
     def __str__(self):
         return self.u_name
+
+
+class project(models.Model):
+    title = models.CharField(max_length =30)
+    project_img = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    link = models.CharField(max_length =30)
+    editor = models.ForeignKey(profile)
+
+
+    def __str__(self):
+        return self.title
