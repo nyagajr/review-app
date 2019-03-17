d66649b91149f6eafeb2901441b582bff38b3402
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 08:17:39 +0300

    adding profile models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    name = models.CharField(max_length =30)
+    dpic = models.CharField(max_length =30)
+    contact_info = models.CharField(max_length =30)
