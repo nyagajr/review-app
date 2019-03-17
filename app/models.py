cad1ccbd02630225f6ecd55c827ed03bfd3c30a0
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-17 21:52:30 +0300

    removing the foreign key

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class profile(models.Model):
     u_name = models.CharField(max_length =30)
-    dpic = models.CharField(max_length =30)
+    # dpic = models.CharField(max_length =30)
     contact_info = models.CharField(max_length =30)
 
     def __str__(self):
@@ -19,10 +19,10 @@ class profile(models.Model):
 
 class project(models.Model):
     title = models.CharField(max_length =30)
-    project_img = models.CharField(max_length =30)
+    # project_img = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
     link = models.CharField(max_length =30)
-    editor = models.ForeignKey(profile, default = 0)
+    editor = models.ForeignKey(profile)
 
     def __str__(self):
         return self.title
