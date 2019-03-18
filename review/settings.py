a5e7249739a1b8519e6e093bcfabedc8767f122e
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-18 14:10:55 +0300

    changing the path of directories

--- review/settings.py
+++ review/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'review.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,11 +66,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
+
         },
     },
 ]
-
 WSGI_APPLICATION = 'review.wsgi.application'
 
 
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
