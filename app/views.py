ccf2b5f0ca17e9bb3833a2684abf04966c980b89
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-18 14:10:16 +0300

    querying projects in views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    new = project.objects.all()
+    
+    return render(request, 'welcome.html',locals())
+
+# def search_results(request):
+#
+#     if 'article' in request.GET and request.GET["article"]:
+#         search_term = request.GET.get("article")
+#         searched_articles = Article.search_by_title(search_term)
+#         message = f"{search_term}"
+#
+#         return render(request, 'all-templates/search.html',{"message":message,"articles": searched_articles})
+#
+#     else:
+#         message = "You haven't searched for any term"
+#         return render(request, 'all-templates/search.html',{"message":message})
