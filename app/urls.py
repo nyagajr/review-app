da1c1ab347906ce55ad4b2dbaf919096755a9318
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-20 11:03:10 +0300

    adding a registration url

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('home',views.welcome,name = 'welcome'),
     # url(r'^search/', views.search_results, name='search_results')
+    url(r'^$',views.signup, name='sign')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
