e527f67088a130fdc08df3e1b53a45a7078d8abd
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-21 10:04:59 +0300

    redefining the Profile model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.TextField(blank = True)
     u_name = models.CharField(max_length =30)
-    dpic = models.ImageField(upload_to = 'articles/')
+    dpic = models.ImageField(default = 'default.jpg',upload_to = 'articles/')
     contact_info = models.CharField(max_length =30)
 
     def __str__(self):
@@ -21,8 +26,8 @@ class project(models.Model):
     title = models.CharField(max_length =30)
     project_img = models.ImageField(upload_to = 'articles/')
     description = models.CharField(max_length =300)
-    link = models.CharField(max_length =30)
-    editor = models.ForeignKey(profile)
+    link = models.URLField(max_length =30)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
